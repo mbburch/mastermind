b27d01922e0921793a449adbc056823c8ccf2f08
MB Burch <marybethburch@gmail.com>
2015-10-16 11:00:13 -0600

    game working. want to refactor

--- mastermind.rb
+++ mastermind.rb
@@ -1,8 +1,9 @@
 class Mastermind
 
-  attr_accessor :guess
+  attr_accessor :guess, :secret_code
 
   def initialize
+    @secret_code = get_code
     puts welcome_message
     initial_input
   end
@@ -17,11 +18,12 @@ class Mastermind
     when 'play', 'p' then play
     when 'instructions', 'i' then instructions
     when 'quit', 'q' then quit
+    else
+      initialize
     end
   end
 
   def play
-    secret_code
     puts "I have generated a beginner sequence with four elements made up of: (r)ed,
     (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game.
     What's your guess?"
@@ -40,69 +42,62 @@ class Mastermind
   end
 
   def take_turn
-    @guess = gets.strip.downcase
-    case guess
-    when 'q', 'quit'
-      quit
-    when 'c', 'cheat'
-      print_secret_code
-    when guess.length < 4
-      puts "Guess must be 4 characters. Guess again."
-    when guess.length > 4
-      puts "Guess must be 4 characters. Guess again."
-    when correct_guess
-      end_game_flow
-    when guess.length == 4
-      puts "#{guess} has #{correct_elements} of the correct_elements with
-       #{correct_placement} in the correct positions. You've taken #{guesses}.
-        Guess again."
-    else
-      raise 'hell'
+    @guess_counter = 0
+    until @guess == ('q' || 'quit') || (correct_guess)
+      @guess = gets.strip.downcase
+      if @guess == ('q' || 'quit')
+        quit
+      elsif @guess == ('c' || 'cheat')
+        print_secret_code
+        puts "Cheater! But go ahead if you must."
+      elsif @guess.length < 4
+        puts "Guess must be 4 characters. Guess again."
+      elsif @guess.length > 4
+        puts "Guess must be 4 characters. Guess again."
+      elsif correct_guess
+        end_game_flow
+      elsif (@guess.length == 4) && (!correct_guess)
+        puts "#{@guess} has #{correct_elements} matching elements with
+         #{correct_placement} in the correct positions. You've taken #{@guess_counter} guesses.
+          Guess again."
+      end
+      @guess_counter += 1
     end
   end
 
-  def secret_code
+  def get_code
     chars = %w(r g b y)
     code_array = [chars.sample, chars.sample, chars.sample, chars.sample]
     code = code_array.join
   end
 
   def print_secret_code
-    puts secret_code
+    puts @secret_code
   end
 
   def correct_elements
     amount = 0
     4.times do |char|
-      amount += 1 if secret_code.include?(guess[char])
+      amount += 1 if @secret_code.include?(@guess[char])
     end
+    amount
   end
 
   def correct_placement
     amount = 0
     4.times do |char|
-      amount += 1 if guess[char] == secret_code[char]
+      amount += 1 if @guess[char] == @secret_code[char]
     end
     amount
   end
 
-  def guesses
-  end
-
   def correct_guess
-    guess == secret_code
+    @guess == @secret_code
   end
 
   def end_game_flow
+    puts "You won!"
   end
 end
 
-
-
-
 mastermind = Mastermind.new
-
-# sanitize input
-# classes to extract
-# how to deal with a guess
-
